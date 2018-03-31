6dc5771e0e4cc50dc39c6d69cfbe9611b27e2d77
CC <ltee12@hotmail.com>
2018-03-31 15:32:00 +0800

    Tile刷新的方式，改为轮询的方式，每500ms检查一次服务状态，改变则更新

--- app/src/main/java/org/kreal/hkshare/QuickSettingService.kt
+++ app/src/main/java/org/kreal/hkshare/QuickSettingService.kt
@@ -4,12 +4,32 @@ import android.annotation.TargetApi
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.os.Handler
 import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 
 @TargetApi(Build.VERSION_CODES.N)
 class QuickSettingService : TileService() {
-    private val serviceConnection = HKService.HKServiceClientBinder()
+    private val serviceConnection: HKService.HKServiceClientBinder = HKService.HKServiceClientBinder()
+
+    private val handler = Handler()
+
+    private val refreshRunnable: Runnable = object : Runnable {
+        override fun run() {
+            when {
+                serviceConnection.isServing() && (qsTile.state == Tile.STATE_INACTIVE) -> {
+                    setActive()
+                    qsTile.updateTile()
+                }
+                !serviceConnection.isServing() && (qsTile.state == Tile.STATE_ACTIVE) -> {
+                    setInactive()
+                    qsTile.updateTile()
+                }
+            }
+            handler.postDelayed(this, 500)
+        }
+    }
+
     override fun onCreate() {
         super.onCreate()
         baseContext.bindService(Intent(baseContext, HKService::class.java), serviceConnection, Context.BIND_AUTO_CREATE)
@@ -21,38 +41,20 @@ class QuickSettingService : TileService() {
     }
 
     override fun onStartListening() {
-//        Log.i("adf", "startinging")
-        super.onStartListening()
-        if (serviceConnection.isServing())
-            setActive()
-        else
-            setInactive()
-        qsTile.updateTile()
+        handler.post(refreshRunnable)
     }
 
     override fun onClick() {
         super.onClick()
         when (serviceConnection.isServing()) {
-            true -> {
-                HKService stopWith applicationContext
-                setInactive()
-            }
-            false -> {
-                HKService startWith applicationContext
-                setActive()
-            }
+            true -> HKService stopWith applicationContext
+            false -> HKService startWith applicationContext
         }
-        qsTile.updateTile()
     }
 
     override fun onStopListening() {
-//        Log.i("adf", "stoping")
         super.onStopListening()
-        if (serviceConnection.isServing())
-            setActive()
-        else
-            setInactive()
-        qsTile.updateTile()
+        handler.removeCallbacks(refreshRunnable)
     }
 
     private fun setActive() {
